bed5c1d53e3c0280a09a3089a77da7a6ac5b71ba
Dinesh Yadav <Dinesh.Yadav@RSK-BSL.COM>
2023-01-04 22:04:00 +0530

    added roles and user_roles through table

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const config = require('./config/serverConfig');
 const bodyParser = require('body-parser');
 const apiRoutes = require('../src/routes/index');
+const db = require('./models/index');
 
 const app = express();
 const prepareAndStartServer = () =>{
@@ -12,7 +13,7 @@ const prepareAndStartServer = () =>{
 
     app.listen(config.PORT,  ()=>{
         console.log(`server has started on Port:${config.PORT}!`);
-
+        //db.sequelize.sync({alter:true});
     })
 }
 
